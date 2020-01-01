8313607af0967356379a4975acd14479ca65ed12
dinilbose <dinilbose@gmail.com>
2020-01-01 21:41:20 +0100

    Intitiation compelete2

--- ionopy/analysis/__init__.py
+++ ionopy/analysis/__init__.py
@@ -2,7 +2,8 @@
 #from ionopy.process import *
 #from ionopy.analysis import *
 #import analysis
-from .analysis import *
+#from .analysis import *
+from ionopy.analysis import analysis
 #from pandas.core.api import *
 #from ._version import get_versions
 
