96242026f66b054f95184c993d1fc74902ef1457
dinilbose <dinilbose@gmail.com>
2020-03-24 12:56:16 +0100

    correction import correction to analysis module

--- ionopy/analysis/__init__.py
+++ ionopy/analysis/__init__.py
@@ -3,7 +3,8 @@
 #from ionopy.analysis import *
 #import analysis
 #from .analysis import *
-from ionopy.analysis import analysis
+from ionopy.analysis.analysis import *
+#from analysis import *
 #from pandas.core.api import *
 #from ._version import get_versions
 
